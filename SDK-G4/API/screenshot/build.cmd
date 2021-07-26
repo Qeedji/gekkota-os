8d29a70fdd4c9ef11dedce2f407ef94462aa8014
innes-labs <labs@innes.fr>
2021-07-26 15:21:23 +0200

    [Screenshot] Access image file with GktFileSystem API

--- SDK-G4/API/screenshot/build.cmd
+++ SDK-G4/API/screenshot/build.cmd
@@ -5,7 +5,7 @@ if not exist dist mkdir dist
 dir /b /a-d *.html > HTML_EXAMPLE_FILES
 for /f "tokens=*" %%a in (HTML_EXAMPLE_FILES) do (
   move %%a app.html
-  tar -cf %%~na.tar app.html manifest.xml logger.js style.css
+  tar -cf %%~na.tar app.html manifest.xml logger.js style.css gkt-filesystem-1.2.1.min.js
   move %%~na.tar dist
   move app.html %%a
   del HTML_EXAMPLE_FILES
